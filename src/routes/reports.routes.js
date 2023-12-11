af220ce118c81ae829144299c147a66047549ed2
Flavio Cabrera <neo.updown@gmai.com>
2023-12-11 14:53:54 -0600

    cambios en ruta reporte

--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -32,4 +32,29 @@ router.get('/day',
     }
 })
 
+// Cambié el método de 'get' a 'post' en las rutas
+router.post('/month',
+    passport.authenticate('jwt', { session: false }),
+    checkRoles('admin'),
+    validatorHandler(queryReportMonthSchema, 'body'), async (req, res, next) => {
+    try {
+        const sales = await service.perMonth(req.body);
+        res.status(200).json(sales);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/day',
+    passport.authenticate('jwt', { session: false }),
+    checkRoles('admin'),
+    validatorHandler(queryReportDaySchema, 'body'), async (req, res, next) => {
+    try {
+        const sales = await service.perDay(req.body);
+        res.status(200).json(sales);
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router
